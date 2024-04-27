6fcdac9444a7f32cbdeb12ead5fe8fb08c452356
zhenhao-ma <bob0103779@gmail.com>
2024-04-27 17:57:21 +0800

    update

--- rag/app/rag.py
+++ rag/app/rag.py
@@ -4,6 +4,11 @@ from werag import WeRag
 
 
 def get_werag(app):
+
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
     folder = os.path.join(app.instance_path, "werag")
     return WeRag(
         persist_directory=folder,
